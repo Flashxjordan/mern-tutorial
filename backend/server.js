2d067413947af12c71601ca684e01bd2c0b45ba8
David Jordan <wordnerd92@gmail.com>
2023-06-22 08:45:10 -0500

    added an .env with a port of 8000

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,6 @@
-//all of our routes, modules, and controllers
-//entry point to our server
-
-//console.log('Hello World')
-
 const express = require('express') //backend framework 
 const dotenv = require('dotenv').config() //enviroment variables 
-const port = 5000
+const port = process.env.PORT || 5000
 
 const app = express()
 
