68e27bb0fe4aafd786c73cfcfc77af5948b3d236
David Jordan <wordnerd92@gmail.com>
2023-06-22 08:41:09 -0500

    created a port for my serever

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
 //all of our routes, modules, and controllers
 //entry point to our server
 
-console.log('Hello World')
+//console.log('Hello World')
+
+const express = require('express') //backend framework 
+const dotenv = require('dotenv').config() //enviroment variables 
+const port = 5000
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
